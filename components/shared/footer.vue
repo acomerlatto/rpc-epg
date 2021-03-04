0b749613335514d57af9b50d67cfd2cdb07f02ac
Alisson Comerlatto <comerlattoalisson@gmail.com>
2021-03-04 02:32:30 -0300

    feat: adiciona componente para footer e altera configurações de estilo do layout

---
+++ components/shared/footer.vue
@@ -0,0 +1,18 @@
+<template>
+  <footer class="text-center text-sm m-5 pb-2">
+    Todos os direitos autorias e materiais reservados aos seus proprietários.<br>
+    &copy; Grupo Paranaense de Comunicação e &copy; Globo Comunicação e Participações S.A<br>
+    <fa class="text-yellow-900" :icon="['fas', 'mug-hot']" /> Desenvolvido por
+    <a class="hover:text-blue-900" href="https://github.com/acomerlatto" target="_blank">Alisson Comerlatto</a>
+  </footer>
+</template>
+
+<script>
+export default {
+  name: 'footer'
+}
+</script>
+
+<style scoped>
+
+</style>
--- layouts/default.vue
+++ layouts/default.vue
@@ -1,11 +1,20 @@
 <template>
   <div>
     <Nuxt />
+    <app-footer />
   </div>
 </template>
 
+<script>
+import AppFooter from '~/components/shared/footer'
+export default {
+  components: { AppFooter }
+}
+
+</script>
+
 <style>
 html {
-  @apply bg-indigo-100
+  @apply bg-indigo-50
 }
 </style>
