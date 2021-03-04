46371d99b48b8af17eca7f36860451833c9b7d72
Alisson Comerlatto <comerlattoalisson@gmail.com>
2021-03-04 02:33:05 -0300

    feat: adiciona componente on-air

---
+++ components/epg/on-air.vue
@@ -0,0 +1,100 @@
+<template>
+  <div ref="onAir">
+    <div class="ao-vivo" :class="{ 'on-body': !topFixed, 'on-top': topFixed }">
+      <img :src="logotipoUrl" alt="Logotipo Agora" class="w-10 hidden sm:block">
+      <div class="w-full md:ml-2">
+        <div class="my-auto flex">
+          <div class="flex-auto">
+            <span class="flex h-3 w-3 inline-flex align-middle">
+              <span class="animate-ping absolute rounded-full h-3 w-3 bg-red-400" />
+              <span class="relative rounded-full h-3 w-3 bg-red-600" />
+            </span>
+            <span class="align-middle text-blue-900 font-bold">
+              <span class="text-red-600 font-normal mr-2">No Ar</span>{{ nomePrograma }}
+            </span>
+          </div>
+          <div class="flex-auto text-right">
+            <span class="align-middle text-red-500">{{ minutosRestantes }} min <span class="hidden md:inline">restante{{ minutosRestantes > 1 ? 's': '' }}</span></span>
+          </div>
+        </div>
+        <div class="shadow w-full bg-gray-100 mt-1 rounded">
+          <div ref="progress-bar" class="bg-red-600 py-1 leading-none text-xs rounded" :style="{ width: barraDeProgresso +'%' }" />
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'EPGOnAir',
+  props: {
+    programa: {
+      type: Object,
+      required: true
+    }
+  },
+  data () {
+    return {
+      timer: null,
+      minutosRestantes: 0,
+      barraDeProgresso: 0,
+      topFixed: false
+    }
+  },
+  computed: {
+    horarioInicio () {
+      return this.$moment.unix(this.programa.start_time).format('HH:mm')
+    },
+    logotipoUrl () {
+      return this.programa.custom_info.Graficos.LogoURL
+    },
+    nomePrograma () {
+      return this.programa.title
+    }
+  },
+  mounted () {
+    this.calcularProgresso()
+    this.calcularPosicao()
+
+    window.addEventListener('scroll', this.calcularPosicao)
+    this.timer = setInterval(this.calcularProgresso, 500)
+  },
+  beforeDestroy () {
+    clearInterval(this.timer)
+    window.removeEventListener('scroll', this.calcularPosicao)
+  },
+  methods: {
+    calcularProgresso () {
+      const fracaoProgresso = this.$moment.duration(this.programa.duration_in_minutes, 'minutes').asMilliseconds() / 100
+
+      const agora = new Date().getTime()
+      const inicioPrograma = this.$moment.unix(this.programa.start_time).utc()
+      const fimPrograma = this.$moment.unix(this.programa.end_time).utc()
+
+      const progresso = (agora - inicioPrograma) / fracaoProgresso
+
+      this.barraDeProgresso = progresso.toFixed(2)
+      this.minutosRestantes = Math.ceil(this.$moment.duration(fimPrograma - agora).asMinutes())
+    },
+    calcularPosicao () {
+      this.topFixed = this.$refs.onAir.getBoundingClientRect().top < 0
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+.ao-vivo {
+  @apply bg-white shadow-md p-2 flex
+}
+
+.ao-vivo.on-body {
+  @apply w-11/12 md:w-10/12 lg:w-2/3 xl:w-1/2 mx-auto rounded
+}
+
+.ao-vivo.on-top {
+  @apply w-full fixed top-0
+}
+</style>
