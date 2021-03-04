ef84a47650b0fbff005924cc291ab9a56086599d
Alisson Comerlatto <comerlattoalisson@gmail.com>
2021-03-04 02:47:41 -0300

    fix: corrige titulo inicial

--- nuxt.config.js
+++ nuxt.config.js
@@ -9,7 +9,7 @@ export default {
 
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
-    title: 'rpc-epg',
+    title: 'RPC - EPG',
     htmlAttrs: {
       lang: 'pt_BR'
     },
