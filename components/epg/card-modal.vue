176bfa953d41a869820b8f959b51984635369096
Alisson Comerlatto <comerlattoalisson@gmail.com>
2021-03-05 19:44:11 -0300

    feat: adiciona modal para exibir detalhes do programa no mobile

--- components/epg/card-modal.vue
+++ components/epg/card-modal.vue
@@ -1,19 +1,15 @@
 <template>
-  <div class="fixed z-10 inset-0 overflow-y-auto" :class="{'hidden' : !detalhes}">
-    <div class="flex items-center justify-center w-full h-full text-center">
-      <span class="hidden sm:inline-block sm:align-middle sm:h-screen" aria-hidden="true">&#8203;</span>
+  <div class="fixed z-10 inset-0 overflow-y-auto">
+    <div class="flex items-center justify-center w-full h-full">
       <div class="inline-block align-bottom bg-white text-left overflow-hidden align-middle w-full h-full" role="dialog" aria-modal="true" aria-labelledby="modal-headline">
-        <div class="h-10 bg-blue-600 text-white">
-          <div class="flex items-center justify-end h-10 pr-2">
-            <div class="flex cursor-pointer" @click="fecharDetalhes">
-              Fechar
-              <svg class="h-6 w-6" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M6 18L18 6M6 6l12 12" />
-              </svg>
-            </div>
+        <div class="absolute top-0 right-0 h-10">
+          <div class="flex cursor-pointer bg-white rounded-full p-1 mt-2 mr-2" @click="$emit('fechar')">
+            <svg class="h-6 w-6" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+              <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M6 18L18 6M6 6l12 12" />
+            </svg>
           </div>
         </div>
-        <img :src="imagem" alt="Imagem" class="flex-none object-cover object-top w-full max-h-48 block">
+        <img :src="imagem" alt="Imagem" class="flex-none object-cover object-top w-full max-h-64 block">
         <div class="bg-white px-4 pt-5 pb-4 sm:p-6 sm:pb-4">
           <div class="sm:flex sm:items-start">
             <div class="mt-3 text-left">
@@ -37,14 +33,16 @@
   </div>
 </template>
 <script>
+import ComputedPrograma from '~/plugins/mixins/epg/programa'
+
 export default {
   name: 'CardModal',
+  mixins: [ComputedPrograma],
   props: {
-    detalhes: {},
-    fecharDetalhes: {},
-    horarioInicio: {},
-    imagem: {},
-    programa: {}
+    programa: {
+      type: Object,
+      required: true
+    }
   }
 }
 </script>
