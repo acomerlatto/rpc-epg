33ca350f56df580ab561503497cc7bc90ae5cba9
Alisson Comerlatto <comerlattoalisson@gmail.com>
2021-03-05 19:43:51 -0300

    feat: efetua ajustes na barra no-ar para se adequar a responsividade

--- components/epg/on-air.vue
+++ components/epg/on-air.vue
@@ -1,24 +1,26 @@
 <template>
   <div ref="onAir">
     <div class="ao-vivo" :class="{ 'on-body': !topFixed, 'on-top': topFixed }">
-      <img :src="logotipoUrl" alt="Logotipo Agora" class="w-10 hidden sm:block">
-      <div class="w-full md:ml-2">
-        <div class="my-auto flex">
-          <div class="flex-auto">
-            <span class="flex h-3 w-3 inline-flex align-middle">
-              <span class="animate-ping absolute rounded-full h-3 w-3 bg-red-400" />
-              <span class="relative rounded-full h-3 w-3 bg-red-600" />
-            </span>
-            <span class="align-middle text-blue-900 font-bold">
-              <span class="text-red-600 font-normal mr-2">No Ar</span>{{ nomePrograma }}
-            </span>
+      <div class="flex w-full mx-auto" :class="{'md:w-10/12': topFixed, 'lg:w-2/3': topFixed, 'xl:w-1/2': topFixed}">
+        <img :src="logotipoUrl" alt="Logotipo Agora" class="w-10 hidden sm:block">
+        <div class="w-full sm:ml-2">
+          <div class="my-auto flex">
+            <div class="flex-auto">
+              <span class="flex h-3 w-3 inline-flex align-middle">
+                <span class="animate-ping absolute rounded-full h-3 w-3 bg-red-400" />
+                <span class="relative rounded-full h-3 w-3 bg-red-600" />
+              </span>
+              <span class="align-middle text-blue-900 font-bold">
+                <span class="text-red-600 font-normal mr-2">No Ar</span>{{ nomePrograma }}
+              </span>
+            </div>
+            <div class="flex-auto text-right">
+              <span class="align-middle text-red-500">{{ minutosRestantes }} min <span class="hidden md:inline">restante{{ minutosRestantes > 1 ? 's': '' }}</span></span>
+            </div>
           </div>
-          <div class="flex-auto text-right">
-            <span class="align-middle text-red-500">{{ minutosRestantes }} min <span class="hidden md:inline">restante{{ minutosRestantes > 1 ? 's': '' }}</span></span>
+          <div class="shadow w-full bg-gray-100 mt-1 rounded">
+            <div ref="progress-bar" class="bg-red-600 py-1 leading-none text-xs rounded" :style="{ width: barraDeProgresso +'%' }" />
           </div>
-        </div>
-        <div class="shadow w-full bg-gray-100 mt-1 rounded">
-          <div ref="progress-bar" class="bg-red-600 py-1 leading-none text-xs rounded" :style="{ width: barraDeProgresso +'%' }" />
         </div>
       </div>
     </div>
@@ -87,7 +89,7 @@ export default {
 <style scoped>
 
 .ao-vivo {
-  @apply bg-white shadow-md p-2 flex
+  @apply bg-white shadow-md p-2
 }
 
 .ao-vivo.on-body {
