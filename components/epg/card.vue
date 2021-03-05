5d8979af0ef5572361f1d44a4faa708a8c3ee70e
Alisson Comerlatto <comerlattoalisson@gmail.com>
2021-03-05 19:44:36 -0300

    feat: efetua alterações no card do programa

--- components/epg/card.vue
+++ components/epg/card.vue
@@ -1,65 +1,89 @@
 <template>
-  <div class="flex rounded w-11/12 md:w-10/12 lg:w-2/3 xl:w-1/2 md:h-40 mx-auto my-4 p-0 bg-white shadow md:shadow-md">
-    <!-- Imagem presente nos dispositivos maiores -->
-    <img :src="imagem" alt="Imagem" class="flex-none object-cover w-56 h-auto hidden md:block md:rounded-l">
+  <div>
+    <div class="detalhes" @click="exibirDetalhes">
+      <!-- Desktpo -->
+      <img :src="imagem" alt="Imagem" class="flex-none object-cover w-56 h-auto hidden md:block md:rounded-l">
+      <div class="ml-3 hidden md:flex md:flex-col md:py-2 md:pr-2">
+        <!-- Titulo -->
+        <div class="hover:text-red-600">
+          <span class="font-semibold text-blue-900">
+            <span class="text-blue-900"><fa :icon="['far', 'clock']" /> {{ horarioInicio }}</span>
+            {{ programa.title }}</span>
+        </div>
 
-    <!--  Logotipo presente no mobile  -->
-    <img :src="logotipo" alt="Logotipo" class="flex-none object-cover rounded-l w-12 md:hidden">
+        <!-- Categorias -->
+        <div class="my-1 hidden md:block">
+          <span class="bg-blue-300 rounded-full py-1 px-2 text-white text-xs">{{ categoria }}</span>
+        </div>
 
-    <!-- Titulo +  Descrição (dispositivos grandes) -->
-    <div class="ml-3 hidden md:flex md:flex-col md:py-2 md:pr-2">
-      <!-- Titulo -->
-      <div class="hover:text-red-600">
-        <span class="font-semibold text-blue-900">
-          <span class="text-blue-900"><fa :icon="['far', 'clock']" /> {{ horarioInicio }}</span>
-          {{ programa.title }}</span>
+        <!-- Descrição -->
+        <div class="text-sm break-words hidden md:block">
+          {{ programa.description }}
+        </div>
       </div>
 
-      <!-- Categorias -->
-      <div class="my-1 hidden md:block">
-        <span class="bg-blue-300 rounded-full py-1 px-2 text-white text-xs">{{ programa.program.category }}</span>
-      </div>
-
-      <!-- Descrição -->
-      <div class="text-sm break-words hidden md:block">
-        {{ programa.description }}
+      <!--  Mobile  -->
+      <img :src="logotipo" alt="Logotipo" class="flex-none object-cover rounded-l w-14 md:hidden">
+      <div class="ml-3 flex flex-wrap content-center md:hidden">
+        <span class="text-blue-900 font-bold">
+          <span class="font-normal"><fa :icon="['far', 'clock']" /> {{ horarioInicio }}</span>
+          {{ programa.title }}
+        </span>
       </div>
     </div>
 
-    <!-- Titulo (mobile) -->
-    <div class="ml-3 flex flex-wrap content-center md:hidden">
-      <span class="text-blue-900 font-bold">
-        <span class="font-normal"><fa :icon="['far', 'clock']" /> {{ horarioInicio }}</span>
-        {{ programa.title }}
-      </span>
-    </div>
+    <!-- Modal para Mobile -->
+    <card-modal v-if="detalhes" :programa="programa" @fechar="fecharDetalhes" />
   </div>
 </template>
 
 <script>
 
+import CardModal from '~/components/epg/card-modal'
+import ComputedPrograma from '~/plugins/mixins/epg/programa'
+
 export default {
   name: 'Card',
+  components: { CardModal },
+  mixins: [ComputedPrograma],
   props: {
     programa: {
       type: Object,
       required: true
     }
   },
-  computed: {
-    imagem () {
-      return this.programa.custom_info.Graficos.ImagemURL || '/images/noimage.png'
+  data () {
+    return {
+      detalhes: false
+    }
+  },
+  methods: {
+    exibirDetalhes () {
+      if (window.innerWidth >= 768) { return }
+
+      this.detalhar()
     },
-    logotipo () {
-      return this.programa.custom_info.Graficos.LogoURL || '/images/noimage.png'
+
+    fecharDetalhes () {
+      this.detalhar()
     },
-    horarioInicio () {
-      return this.$moment.unix(this.programa.start_time).format('HH:mm')
+    detalhar () {
+      const root = document.getElementsByTagName('html').item(0)
+
+      if (this.detalhes) {
+        root.classList.remove('overflow-hidden')
+      } else {
+        root.classList.add('overflow-hidden')
+      }
+
+      this.detalhes = !this.detalhes
     }
   }
 }
 </script>
 
-<style scoped>
-
+<style>
+.detalhes {
+  @apply flex rounded w-11/12 md:w-10/12 lg:w-2/3 xl:w-1/2 md:h-40 mx-auto my-4 p-0 bg-white shadow md:shadow-md cursor-pointer md:cursor-default
+}
 </style>
