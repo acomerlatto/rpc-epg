69ad4dbc0bea7324060e6468514fed8eda6ccbb9
Alisson Comerlatto <comerlattoalisson@gmail.com>
2021-03-05 19:45:01 -0300

    feat(mixin): adiciona mixin para informações de um programa

---
+++ plugins/mixins/epg/programa.js
@@ -0,0 +1,16 @@
+export default {
+  computed: {
+    imagem () {
+      return this.programa.custom_info.Graficos.ImagemURL || '/images/noimage.png'
+    },
+    logotipo () {
+      return this.programa.custom_info.Graficos.LogoURL || '/images/noimage.png'
+    },
+    horarioInicio () {
+      return this.$moment.unix(this.programa.start_time).format('HH:mm')
+    },
+    categoria () {
+      return this.programa.program.category
+    }
+  }
+}
