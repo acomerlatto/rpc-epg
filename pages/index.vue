850e0aad49d5a9f20fcb234edb4c13e703b89106
Alisson Comerlatto <comerlattoalisson@gmail.com>
2021-03-05 21:28:49 -0300

    feat: adiciona navegação por dias

--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,23 @@
 <template>
   <div class="py-6">
+    <div class="flex-auto justify-center items-center flex space-x-4 mb-6">
+      <button class="btn btn-rounded btn-primary-outline hidden md:flex" @click="baixarNovoGuia($moment(dataProgramacao).subtract(2, 'day'))">
+        {{ $moment(dataProgramacao).subtract(2, 'day').format('DD/MM') }}
+      </button>
+      <button class="btn btn-rounded btn-primary-outline" @click="baixarNovoGuia($moment(dataProgramacao).subtract(1, 'day'))">
+        {{ $moment(dataProgramacao).subtract(1, 'day').format('DD/MM') }}
+      </button>
+      <div class="btn btn-rounded bg-blue-900 text-white text-center">
+        {{ dataProgramacao.isSame($moment(), 'date') ? 'Hoje' : dataProgramacao.format('DD/MM') }}
+      </div>
+      <button class="btn btn-rounded btn-primary-outline" @click="baixarNovoGuia($moment(dataProgramacao).add(1, 'day'))">
+        {{ $moment(dataProgramacao).add(1, 'day').format('DD/MM') }}
+      </button>
+      <button class="btn btn-rounded btn-primary-outline hidden md:flex" @click="baixarNovoGuia($moment(dataProgramacao).add(2, 'day'))">
+        {{ $moment(dataProgramacao).add(2, 'day').format('DD/MM') }}
+      </button>
+    </div>
+
     <div class="mb-6 text-center">
       <img ref="logotipo" class="logotipo" src="/images/logo.png" alt="Logotipo RPC">
       <h2 class="text-blue-900 text-2xl capitalize font-bold">
@@ -10,7 +28,7 @@
     <app-on-air v-if="noAr && programaAtual" ref="on-air" class="my-8" :programa="programaAtual" />
 
     <div class="mt-6">
-      <app-card-detalhes v-for="(programa, index) in programas" :key="index" :programa="programa" :programaAtual="programa === programaAtual" />
+      <app-card-detalhes v-for="(programa, index) in programas" :key="index" :programa="programa" :programa-atual="programa === programaAtual" />
     </div>
   </div>
 </template>
@@ -27,10 +45,10 @@ export default {
   },
   async asyncData ({ $axios, $moment }) {
     const dataProgramacao = $moment()
-    const epgToday = await $axios.$get(`/epg?date=${dataProgramacao.format('YYYY-MM-DD')}`)
-    const epgYesterday = await $axios.$get(`/epg?date=${$moment(dataProgramacao).subtract(1, 'day').format('YYYY-MM-DD')}`)
+    const epgInicial = await $axios.$get(`/epg?date=${dataProgramacao.format('YYYY-MM-DD')}`)
+    const epgFinal = await $axios.$get(`/epg?date=${$moment(dataProgramacao).subtract(1, 'day').format('YYYY-MM-DD')}`)
 
-    return { epg: epgToday.programme.entries.concat(epgYesterday.programme.entries), dataProgramacao }
+    return { epg: epgInicial.programme.entries.concat(epgFinal.programme.entries), dataProgramacao }
   },
   data () {
     return {
@@ -46,8 +64,8 @@ export default {
   computed: {
     programas () {
       const $this = this
-      const inicioHoje = $this.$moment().startOf('day')
-      const fimHoje = $this.$moment().endOf('day')
+      const inicioHoje = $this.$moment(this.dataProgramacao).startOf('day')
+      const fimHoje = $this.$moment(this.dataProgramacao).endOf('day')
 
       return this.epg.filter(elemento => $this.$moment.unix(elemento.start_time).isBetween(inicioHoje, fimHoje))
         .sort(function (a, b) {
@@ -57,13 +75,6 @@ export default {
 
           return 0
         })
-    },
-    imagem () {
-      return this.programaAtual.custom_info.Graficos.ImagemURL
-    },
-
-    horarioInicio () {
-      return this.$moment.unix(this.programaAtual.start_time).format('HH:mm')
     },
 
     noAr () {
@@ -84,6 +95,13 @@ export default {
 
         return $this.$moment().isBetween(inicio, fim)
       })
+    },
+    async baixarNovoGuia (data) {
+      const epgInicial = await this.$axios.$get(`/epg?date=${data.format('YYYY-MM-DD')}`)
+      const epgFinal = await this.$axios.$get(`/epg?date=${this.$moment(data).subtract(1, 'day').format('YYYY-MM-DD')}`)
+
+      this.dataProgramacao = data
+      this.epg = epgInicial.programme.entries.concat(epgFinal.programme.entries)
     }
   }
 }
@@ -92,5 +110,15 @@ export default {
 <style>
 .logotipo {
   @apply mx-auto w-64 h-auto
+}
+
+.btn {
+  @apply py-2 w-36 items-center justify-center font-bold
+}
+.btn-rounded {
+  @apply rounded-full
+}
+.btn-primary-outline {
+  @apply border border-blue-900 text-blue-900 hover:border-blue-800 hover:text-blue-800
 }
 </style>
